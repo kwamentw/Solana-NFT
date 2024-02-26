296900bf7f5308d375a2e75f80595b5e532aca7a
KWAME <antwi1496@hotmail.com>
2024-02-25 19:00:54 -0600

    Writing, Deploying and Testing Contracts

--- programs/solana-nft/src/lib.rs
+++ programs/solana-nft/src/lib.rs
@@ -3,15 +3,12 @@ use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{
-        create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
-        CreateMetadataAccountsV3, Metadata, MetadataAccount,
+        create_master_edition_v3, create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMasterEditionV3,
+        CreateMetadataAccountsV3, Metadata,
     }, //new
-    state::DataV2,
     token::{mint_to, Mint, MintTo, Token, TokenAccount},
 };
-use mpl_token_metadata::{
-    pda::{find_master_edition_account, find_metadata_account}, //new
-};
+use mpl_token_metadata::accounts::{MasterEdition,Metadata as MetadataAccount}; //new
 
 declare_id!("9gcy363E4e8to6GCVje5xxLfqe6krVFfot2HHymteCod");
 
@@ -20,7 +17,7 @@ declare_id!("9gcy363E4e8to6GCVje5xxLfqe6krVFfot2HHymteCod");
 pub mod solana_nft {
     use super::*;
 
-    pub fn InitNFT(ctx: Context<InitNFT>) -> Result<()> {
+    pub fn init_nft(ctx: Context<InitNFT>, name:String, symbol:String, uri:String,) -> Result<()> {
         // creating mint account
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
@@ -33,7 +30,22 @@ pub mod solana_nft {
         mint_to(cpi_context, 1)?;
 
         // create metadata account
-        let cpi_context=CpiContext::new(ctsx.accounts.token_metadata_program.to_account_info(), CreateMetadataAccountsV3{metadata:ctx.accounts.metadata_account.accoun})
+        let cpi_context=CpiContext::new(ctx.accounts.token_metadata_program.to_account_info(), CreateMetadataAccountsV3{metadata:ctx.accounts.metadata_account.to_account_info(), mint:ctx.accounts.mint.to_account_info(),mint_authority:ctx.accounts.signer.to_account_info(),update_authority:ctx.accounts.signer.to_account_info(),payer:ctx.accounts.signer.to_account_info(),system_program:ctx.accounts.system_program.to_account_info(),rent:ctx.accounts.rent.to_account_info(),},);
+        let data_v2 = DataV2{
+            name: name,
+            symbol: symbol,
+            uri: uri,
+            seller_fee_basis_points:0,
+            creators: None,
+            collection: None,
+            uses: None, 
+        };
+
+        create_metadata_accounts_v3(cpi_context, data_v2,false,true,None)?;
+
+        //create master edition account
+        let cpi_context = CpiContext::new(ctx.accounts.token_metadata_program.to_account_info(), CreateMasterEditionV3{edition:ctx.accounts.master_edition_account.to_account_info(), mint:ctx.accounts.mint.to_account_info(), update_authority: ctx.accounts.signer.to_account_info(),mint_authority: ctx.accounts.signer.to_account_info(),payer: ctx.accounts.signer.to_account_info(), metadata: ctx.accounts.metadata_account.to_account_info(),token_program: ctx.accounts.token_program.to_account_info(),system_program: ctx.accounts.system_program.to_account_info(), rent: ctx.accounts.rent.to_account_info(),},);
+        create_master_edition_v3(cpi_context, None)?;
 
         Ok(())
     }
@@ -56,15 +68,16 @@ pub struct InitNFT<'info> {
     // initialising token account with account type since it has many constraints
     pub associated_token_account: Account<'info, TokenAccount>,
     /// CHECK - address
-    #[account(mut, address=find_metadata_account(&mint.key()).0,)]
+    #[account(mut, address=MetadataAccount::find_pda(&mint.key()).0,)]
     pub metadata_account: AccountInfo<'info>,
     /// CHECK - address
-    #[account(mut,address=find_master_edition_account(&mint.key()).0,)]
+    #[account(mut,address=MasterEdition::find_pda(&mint.key()).0,)]
     pub master_edition_account: AccountInfo<'info>,
 
     /////////////////////////--------program declaration--------//////////////////////////////
     pub token_program: Program<'info, Token>, // new token program kinda like the mastermind
     pub associated_token_program: Program<'info, AssociatedToken>, // new Associated token program handles the creation of an associated token account (ata)
+    pub token_metadata_program: Program<'info, Metadata>,
     pub system_program: Program<'info, System>, // new system program responsible for creating all accounts
     pub rent: Sysvar<'info, Rent>, // new rent program responsible for paying rent logic
 }
