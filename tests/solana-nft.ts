de9c394d14685a806ccc22313d64095d2b389587
KWAME <antwi1496@hotmail.com>
2024-03-04 18:39:58 -0600

    Writing, Deploying and Testing Contracts

--- tests/solana-nft.ts
+++ tests/solana-nft.ts
@@ -1,12 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaNftAnchor } from "../target/types/solana_nft_anchor";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { findMasterEditionPda, findMetadataPda, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { createUmi, publicKey } from "@metaplex-foundation/umi";
 
-describe("solana-nft", () => {
+describe("solana-nft", async () => {
   // Configure the client to use the local cluster.
   const provider =anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.SolanaNftAnchor as Program<SolanaNftAnchor>;
+
+  const signer = provider.wallet;
+
+  const umi = createUmi("https://api.devnet.solana.com").use(walletAdapterIdentity(signer)).use(mplTokenMetadata());
+
+  //generating our mint account
+  const mint = anchor.web3.Keypair.generate();
+
+  //Derive the associated token address for the mint
+  const associatedTokenAccount = await getAssociatedTokenAddress(mint.publicKey,signer.publicKey);
+  
+  //Derive the metadata account
+  let metadataAccount=findMetadataPda(umi,{mint:publicKey(mint.publicKey)})[0];
+
+  // derive the master edition pda
+  let masterEditionAccount = findMasterEditionPda(umi,{mint: publicKey(mint.publicKey)})[0];
+  
 
 
   it("Is initialized!", async () => {
